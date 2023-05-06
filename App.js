57f99059f2621459e6da8a2ba23a28d57e9b025f
franciele <francielefernandes126@gmail.com>
2023-05-06 19:23:54 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#87CEEB",
+      }}
+    >
+      <Text
+      style={{
+        fontFamily: "Roboto",
+        fontSize: 30,
+        color: "#fff",
+      }}>Franciele</Text>
+    </View>
+  );
+}
